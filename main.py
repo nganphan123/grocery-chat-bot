f9ee479f4bc643d71dea18e7d09596eded1c2c24
nganphan123 <jill01009@gmail.com>
2022-04-12 01:34:23 -0700

    Deploy to code heroku

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from app.bot.bot import Bot
-# from dialogflow_migration.bot import Bot
+from urllib import response
+from flask import Flask, render_template, request
 
-def main():
-    bot = Bot()
-    #initiate conversation with customer
-    bot.start_conversation()
+from app.response.handle_intent import handle_intent
 
+app = Flask(__name__,template_folder='./app/pages')
+
+@app.route("/", methods = ['GET', 'POST'])
+def index():
+    return "<h1>Seashore seashell shop</h1>"
+
+@app.route('/webhook', methods = ['POST', 'GET'])
+def webhook():
+    return handle_intent(request)
+
+@app.route('/order-success', methods = ['GET','POST'])
+def order_success():
+    # handle payment execute
+    return render_template('confirm-payment.html')
 
 if __name__ == '__main__':
-  main()
+    app.run()
